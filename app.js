815902fdb070d9ca43f57ba43c5af035485220d7
Padchara Teeduang <jab-love@hotmail.co.th>
2018-11-14 13:59:02 +0700

    Docker

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
+const path=require('path')
+const ejs=require('ejs')
 const express = require('express')
 const morgan = require('morgan')
+const todos = require("./routes/todos")
 const app = express()
 const port = process.env.Port||3000
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use("/todos",todos)
 if(app.get('env')==='development'){
     app.use(morgan('dev'))
 }
+app.use(express.static('public'))
+// app.get('/',(req,res)=>{
+//     res.send("Hello World")
+// })
 
-
-app.get('/',(req,res)=>{
-    res.send("Hello World")
-})
 app.post('/',(req,res)=>{
     res.send('Got a POST request')
 })
@@ -34,6 +38,11 @@ res.send(`Hello ${name}`)
 app.post('/message',function (req,res){
     console.log(req.body)
     res.send('Got a POST request')
+})
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+app.get('/',(req,res)=>{
+    res.render('index',{name:"Padchara"})
 })
 
 app.listen(port,()=>{
