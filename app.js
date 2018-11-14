cc4cca13c2b46d010d712163f34d535f778be85e
Padchara Teeduang <jab-love@hotmail.co.th>
2018-11-14 13:09:51 +0700

    Basic routing

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const port = process.env.Port||3000
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
+app.post('/',(req,res)=>{
+    res.send('Got a POST request')
+})
+app.put('/user',function(req,res){
+res.send('Got a PUT request at /user')
+})
+app.delete('/user',function(req,res){
+    res.send('Got a DELETE request at /user')
+})
 app.listen(port,()=>{
  console.log(`Server listen on port ${port}`)
 })
